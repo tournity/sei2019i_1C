f8d6c7e28cdb867aedc121953c360c7cf61b23e6
unknown <afsanchezsa@unal.edu.co>
2019-07-02 20:58:25 -0500

    Se crea la clase Tournament Model y se inserta el metodo getAll en TournamentModel

---
+++ android/app/src/main/java/com/tournity/DataModels/TournamentModel.java
@@ -0,0 +1,44 @@
+package com.tournity.DataModels;
+
+import android.content.Context;
+
+import com.tournity.DataAccess.Repositories.TournamentRepository;
+import com.tournity.Entities.TournamentEntity;
+
+import java.util.ArrayList;
+
+public class TournamentModel {
+    private Context context;
+    private TournamentEntity tournamentEntity;
+    private TournamentRepository tournamentRepository;
+    public TournamentModel(Context context) {
+        this.context = context;
+        this.tournamentRepository=new TournamentRepository();
+    }
+
+    public TournamentModel() {
+        this.tournamentRepository=new TournamentRepository();
+    }
+
+    public TournamentModel(TournamentEntity tournamentEntity) {
+        this.tournamentEntity = tournamentEntity;
+        this.tournamentRepository=new TournamentRepository();
+    }
+
+    public TournamentEntity getTournamentEntity() {
+        return tournamentEntity;
+    }
+
+    public void setTournamentEntity(TournamentEntity tournamentEntity) {
+        this.tournamentEntity = tournamentEntity;
+    }
+
+    public ArrayList<TournamentModel>getAll(){
+        ArrayList<TournamentModel>result=new ArrayList<>();
+        for(TournamentEntity tournament:this.tournamentRepository.getAll()){
+            result.add(new TournamentModel(tournament));
+        }
+       return result;
+
+    }
+}
