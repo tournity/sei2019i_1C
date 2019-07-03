ba0721720b218cd6d8816b95647f3b3d175e6c9b
unknown <afsanchezsa@unal.edu.co>
2019-07-03 17:53:57 -0500

    se inserta el boton de ViewTournaments y se declaran las variabes de widget en ViewTournament

--- android/app/src/main/java/com/tournity/View/Activities/ViewTournaments.java
+++ android/app/src/main/java/com/tournity/View/Activities/ViewTournaments.java
@@ -3,14 +3,27 @@ package com.tournity.View.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.tournity.R;
 
 public class ViewTournaments extends AppCompatActivity {
-
+private EditText txtOwner;
+private Button btnSearch;
+private Spinner Filter;
+private Spinner FilterValue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_tournaments);
+
+
+         txtOwner=findViewById(R.id.txtOwner);
+         Filter=(Spinner) findViewById(R.id.SpinnerTypeFilter);
+         FilterValue=(Spinner) findViewById(R.id.SpinnerFilterValue);
+         btnSearch=(Button)findViewById(R.id.btnSearch);
+         
     }
 }
