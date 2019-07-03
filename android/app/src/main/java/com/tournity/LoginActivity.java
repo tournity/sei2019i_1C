faa9c56e5af120b556c5d7bcfd658570d74799a6
Gonzalo Diaz <egonzalodm@gmail.com>
2019-07-02 23:49:47 -0500

    activity

---
+++ android/app/src/main/java/com/tournity/LoginActivity.java
@@ -0,0 +1,43 @@
+package com.tournity;
+
+import android.app.Activity;
+
+import android.os.Bundle;
+
+
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.tournity.R;
+
+public class LoginActivity{
+
+
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
+                .get(LoginViewModel.class);
+
+        final EditText usernameEditText = findViewById(R.id.username);
+        final EditText passwordEditText = findViewById(R.id.password);
+        final Button loginButton = findViewById(R.id.login);
+        final ProgressBar loadingProgressBar = findViewById(R.id.loading);
+
+
+
+    }
+
+
+
+
+}
