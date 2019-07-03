bea0d555f3631597ce2c931f0c9c14881ffa3449
Heldeg <edhernandezfo@unal.edu.co>
2019-07-03 18:24:46 -0500

    mapeo playerEntity

--- android/app/src/main/java/com/tournity/Entities/PlayerEntity.java
+++ android/app/src/main/java/com/tournity/Entities/PlayerEntity.java
@@ -2,8 +2,24 @@ package com.tournity.Entities;
 
 import android.graphics.Bitmap;
 
-import java.util.ArrayList;
-
 public class PlayerEntity {
     private int id;
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+    private Bitmap photo;
+    private TeamEntity group;
+
+    private PlayerEntity(int id, String name, int age,double height,double wight, Bitmap photo,TeamEntity group){
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = wight;
+        this.photo = photo;
+        this.group = group;
+    }
+
+
 }
