65326d819dc3a65d804deb245993b94077561641
unknown <afsanchezsa@unal.edu.co>
2019-07-03 21:06:54 -0500

    se crea el metodo selectAll en SportRepository

--- android/app/src/main/java/com/tournity/Repository/Repositories/SportRepository.java
+++ android/app/src/main/java/com/tournity/Repository/Repositories/SportRepository.java
@@ -1,4 +1,27 @@
 package com.tournity.Repository.Repositories;
 
+import android.content.Context;
+
+import com.tournity.Entities.SportEntity;
+
+import java.util.ArrayList;
+
 public class SportRepository {
+    private Context context;
+
+    public SportRepository(Context context) {
+        this.context = context;
+    }
+
+    public SportRepository() {
+    }
+public ArrayList<SportEntity>selectAll(){
+
+        ArrayList<SportEntity>result=new ArrayList<>();
+        
+
+
+        return result;
+
+}
 }
