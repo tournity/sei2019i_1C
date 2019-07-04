42daa83bb570ae67d38b101a8aa24fde875337a3
Francisco Lugo <jlugon@unal.edu.co>
2019-07-03 22:40:49 -0500

    Create Tournament layout

---
+++ android/app/src/main/java/com/tournity/View/Activities/CreateTournament.java
@@ -0,0 +1,15 @@
+package com.tournity.View.Activities;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tournity.R;
+
+public class CreateTournament extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_tournament);
+    }
+}
