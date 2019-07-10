5e44096141046955f42da9668b19ec9a0f274dd1
unknown <afsanchezsa@unal.edu.co>
2019-07-09 19:21:07 -0500

    se crea la actividad RegisterActivity y el UserController

---
+++ android/app/src/main/java/com/tournity/Bloc/Controllers/UserController.java
@@ -0,0 +1,14 @@
+package com.tournity.Bloc.Controllers;
+
+import android.content.Context;
+
+public class UserController {
+    private Context context;
+
+    public UserController(Context context) {
+        this.context = context;
+    }
+
+
+    
+}
---
+++ android/app/src/main/java/com/tournity/View/Activities/RegisterActivity.java
@@ -0,0 +1,44 @@
+package com.tournity.View.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tournity.Bloc.Controllers.UserController;
+import com.tournity.Bloc.Enums.ControllerError;
+import com.tournity.Bloc.Listeners.ControllerListener;
+import com.tournity.R;
+
+public class RegisterActivity extends AppCompatActivity {
+EditText txtUsername;
+EditText txtEmail;
+EditText txtPassword;
+EditText txtPasswordConfirm;
+UserController controller;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        txtUsername=(EditText)findViewById(R.id.txtUsername);
+        txtEmail=(EditText)findViewById(R.id.txtEmail);
+        txtPassword=(EditText)findViewById(R.id.txtPassword);
+        txtPasswordConfirm=(EditText)findViewById(R.id.txtPasswordConfirm);
+   controller
+    }
+    public void Register(View v){
+        ControllerListener listener=new ControllerListener() {
+            @Override
+            public void Then() {
+
+            }
+
+            @Override
+            public void Catch(ControllerError error) {
+
+            }
+        };
+
+    }
+}
