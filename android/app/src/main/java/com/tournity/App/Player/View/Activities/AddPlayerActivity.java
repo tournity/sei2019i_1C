ce0e7c1bc4a1e50ac672c03c1882f52468d04b7e
Gonzalo Diaz <egonzalodm@gmail.com>
2019-07-11 01:35:04 -0500

    Activities

--- android/app/src/main/java/com/tournity/App/Player/View/Activities/AddPlayerActivity.java
+++ android/app/src/main/java/com/tournity/App/Player/View/Activities/AddPlayerActivity.java
@@ -1,4 +1,26 @@
 package com.tournity.App.Player.View.Activities;
 
-public class AddPlayerActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.tournity.R;
+
+public class AddPlayerActivity extends AppCompatActivity {
+    Button btnAddPlayer;
+
+    protected void onCreate(Bundle bundleSavedInstance) {
+        super.onCreate(bundleSavedInstance);
+        setContentView(R.layout.activity_add_player);
+        btnAddPlayer = findViewById(R.id.btn_add_player);
+        btnAddPlayer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
 }
