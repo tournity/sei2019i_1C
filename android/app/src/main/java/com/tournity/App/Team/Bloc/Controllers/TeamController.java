2192f34ce6362b9450760ee11b8faec33caaa3b3
Gonzalo Diaz <egonzalodm@gmail.com>
2019-07-16 11:48:35 -0500

    Home

--- android/app/src/main/java/com/tournity/App/Team/Bloc/Controllers/TeamController.java
+++ android/app/src/main/java/com/tournity/App/Team/Bloc/Controllers/TeamController.java
@@ -4,13 +4,13 @@ import android.content.Context;
 import android.content.Intent;
 import android.widget.Toast;
 
+import com.tournity.App.Team.Repository.Models.TeamModel;
 import com.tournity.App.Team.View.Activities.CreateTeamActivity;
 import com.tournity.App.Team.View.Activities.TeamsActivity;
 import com.tournity.Bloc.Enums.ControllerError;
 import com.tournity.Bloc.Listeners.TeamListener;
 import com.tournity.Repository.Enums.ModelError;
 import com.tournity.Repository.Listeners.ModelListener;
-import com.tournity.Repository.Models.TeamModel;
 
 public class TeamController {
     Context ctx;
