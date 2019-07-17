aff5975f8851c3fbb1b1d8a5c65f8013c0a2e430
Gonzalo Diaz <egonzalodm@gmail.com>
2019-07-16 20:25:07 -0500

    update

--- android/app/src/main/java/com/tournity/View/Activities/HomeActivity.java
+++ android/app/src/main/java/com/tournity/View/Activities/HomeActivity.java
@@ -1,18 +1,24 @@
 package com.tournity.View.Activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tournity.R;
+import com.tournity.View.Fragments.SportFragment;
+import com.tournity.View.Fragments.TournamentFragment;
+import com.tournity.View.Fragments.UserFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTabHost;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements TournamentFragment.OnFragmentInteractionListener{
     private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -23,16 +29,22 @@ public class HomeActivity extends AppCompatActivity {
             Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_tournament);
+                    selectedFragment = TournamentFragment.newInstance();
+                    //mTextMessage.setText(R.string.title_tournament);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_sport);
+                    selectedFragment= SportFragment.newInstance();
+                    //mTextMessage.setText(R.string.title_sport);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_profile);
+                    selectedFragment = UserFragment.newInstance();
+                   // mTextMessage.setText(R.string.title_profile);
                     return true;
             }
-            return false;
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout,selectedFragment);
+            transaction.commit();
+            return true;
         }
     };
 
@@ -43,6 +55,13 @@ public class HomeActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, TournamentFragment.newInstance());
+        transaction.commit();
     }
 
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
 }
