a8a6fbe109281baa0606f0a30060fdff085a82bb
unknown <afsanchezsa@unal.edu.co>
2019-07-18 18:07:00 -0500

    se agrega vista de partidos MatchViewActivity

---
+++ android/app/src/main/java/com/tournity/App/Match/View/Fragments/MatchViewActivity.java
@@ -0,0 +1,25 @@
+package com.tournity.App.Match.View.Fragments;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.tournity.R;
+
+public class MatchViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_match_view);
+
+
+    }
+
+}
