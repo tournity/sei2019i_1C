8d80b831c2e352b6bed54c9b4e5b5ef8c097c511
Gonzalo Diaz <egonzalodm@gmail.com>
2019-07-18 12:13:04 -0500

    update

--- android/app/src/main/java/com/tournity/App/Group/View/Activities/ListGroupActivity.java
+++ android/app/src/main/java/com/tournity/App/Group/View/Activities/ListGroupActivity.java
@@ -3,6 +3,7 @@ package com.tournity.App.Group.View.Activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +11,35 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.tournity.App.Group.Bloc.Controllers.GroupController;
 import com.tournity.R;
 
+import java.util.ArrayList;
+
 public class ListGroupActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     public ListView listView;
     GroupController controller;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        listView = findViewById(R.id.idListSport);
+        setContentView(R.layout.activity_view_groups);
+        listView = findViewById(R.id.idDataUser);
+        controller = new GroupController(this);
         controller.listGroups();
-        listView.setOnItemClickListener(this);
+
+        //----Test---------------------------------------------
+        ArrayList<String> listGroupName = new ArrayList<>();
+        listGroupName.add("grupo 1");
+        listGroupName.add("grupo 2");
+        ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_expandable_list_item_1, listGroupName);
+        listView.setAdapter(adapter);
+        //----------------------------------------------------
+
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+        });
 
     }
 
