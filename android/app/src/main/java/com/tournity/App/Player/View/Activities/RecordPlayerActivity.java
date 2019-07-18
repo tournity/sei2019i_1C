e8254fb207cd1ea08a3e05cd827b8421e1301dd5
Heldeg <edhernandezfo@unal.edu.co>
2019-07-18 14:45:47 -0500

    avance actividades

--- android/app/src/main/java/com/tournity/App/Player/View/Activities/RecordPlayerActivity.java
+++ android/app/src/main/java/com/tournity/App/Player/View/Activities/RecordPlayerActivity.java
@@ -2,7 +2,9 @@ package com.tournity.App.Player.View.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.tournity.R;
 
@@ -12,5 +14,10 @@ public class RecordPlayerActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_player);
+    }
+
+    public void onClick(View view) {
+        Intent myIntent = new Intent(RecordPlayerActivity.this,EditPlayerProfileActivity.class);
+        startActivity(myIntent);
     }
 }
