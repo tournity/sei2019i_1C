44bc61170281e6d505be5678de496d6ef1d3b942
Gonzalo Diaz <egonzalodm@gmail.com>
2019-07-18 04:37:48 -0500

    nav bar

--- android/app/src/main/java/com/tournity/View/Activities/HomeActivity.java
+++ android/app/src/main/java/com/tournity/View/Activities/HomeActivity.java
@@ -11,39 +11,44 @@ import com.tournity.View.Fragments.UserFragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTabHost;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
 
-public class HomeActivity extends AppCompatActivity implements TournamentFragment.OnFragmentInteractionListener{
-    private TextView mTextMessage;
+public class HomeActivity extends AppCompatActivity implements TournamentFragment.OnFragmentInteractionListener,
+        SportFragment.OnFragmentInteractionListener, UserFragment.OnFragmentInteractionListener {
+
+    private SportFragment sportFragment;
+    private TournamentFragment tournamentFragment;
+    private UserFragment userFragment;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
+            // Fragment selectedFragment = null;
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            System.out.println("Pressed " + item.getItemId());
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    selectedFragment = TournamentFragment.newInstance();
-                    //mTextMessage.setText(R.string.title_tournament);
-                    return true;
-                case R.id.navigation_dashboard:
-                    selectedFragment= SportFragment.newInstance();
-                    //mTextMessage.setText(R.string.title_sport);
-                    return true;
-                case R.id.navigation_notifications:
-                    selectedFragment = UserFragment.newInstance();
-                   // mTextMessage.setText(R.string.title_profile);
-                    return true;
+                case R.id.navigation_tournament:
+                    //selectedFragment = TournamentFragment.newInstance();
+                    System.out.println("tournament");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, tournamentFragment).commit();
+                case R.id.navigation_sport:
+                    //selectedFragment= SportFragment.newInstance();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, sportFragment).commit();
+                    System.out.println("sport");
+
+                case R.id.navigation_profile:
+                    //selectedFragment = UserFragment.newInstance();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, userFragment).commit();
+                    System.out.println("profile");
             }
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.frame_layout,selectedFragment);
-            transaction.commit();
+            /*FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.frame_layout,selectedFragment);*/
+            fragmentTransaction.commit();
             return true;
         }
     };
@@ -51,17 +56,25 @@ public class HomeActivity extends AppCompatActivity implements TournamentFragmen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        sportFragment = new SportFragment();
+        tournamentFragment = new TournamentFragment();
+        userFragment = new UserFragment();
         setContentView(R.layout.activity_home);
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        getSupportFragmentManager().beginTransaction().add(R.id.frame_layout, tournamentFragment).commit();
+
+
+       /* FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.frame_layout, TournamentFragment.newInstance());
-        transaction.commit();
+        transaction.commit();*/
     }
 
     @Override
     public void onFragmentInteraction(Uri uri) {
 
+    }
+
+    public void onClick(View view) {
     }
 }
