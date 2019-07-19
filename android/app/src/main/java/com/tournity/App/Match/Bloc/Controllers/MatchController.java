841f071aae81160e8be02c08200556721490206e
unknown <afsanchezsa@unal.edu.co>
2019-07-19 17:37:41 -0500

    se crea metodo getByIdTournament en MatchModel

---
+++ android/app/src/main/java/com/tournity/App/Match/Bloc/Controllers/MatchController.java
@@ -0,0 +1,17 @@
+package com.tournity.App.Match.Bloc.Controllers;
+
+import android.content.Context;
+
+import com.tournity.App.Match.Repository.Models.MatchModel;
+
+public class MatchController {
+    private Context context;
+    private MatchModel matchModel;
+
+    public MatchController(Context context) {
+        this.context = context;
+        this.matchModel=new MatchModel(context);
+    }
+
+ public static void
+}
